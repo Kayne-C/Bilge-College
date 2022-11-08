18180bfe2b3c43e72a3228abc83e8f7f29941f02
Kaan Cemre Mir <kaancemremir@LIVE.COM>
2022-11-08 15:24:28 +0300

    one of those days..

---
+++ Bilge College/TagHelpers/SaveButtonTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Bilge_College.TagHelpers
+{
+    [HtmlTargetElement("button-save")]
+    public class SaveButtonTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "button";
+            output.Attributes.Add("type", "submit");
+            output.Attributes.Add("class", "btn btn-success");
+            output.Attributes.Add("value", "Kaydet");
+            output.Content.AppendHtml("<i class='fa fa-save'></i> Kaydet");
+            base.Process(context, output);
+        }
+    }
+}
